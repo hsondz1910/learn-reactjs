543f22cc012558aff68fb7f4e6288012f8c96609
Max Noah <hsondz1910@gmail.com>
2024-07-02 19:11:22 +0700

    - Apply useReducer instead of useState or useState following object.

--- src/components/tictactoe/Game.js
+++ src/components/tictactoe/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import Board from './Board';
 import "./GameStyles.css";
 import { calculateWinner } from '../../helpers';
@@ -6,32 +6,90 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container';
 
+const initialState = {
+    board: Array(9).fill(null),
+    xIsNext: true,
+};
+
+const gameReducer = (state, action) => {
+    switch (action.type) {
+        case 'CLICK':
+            {
+                // console.log("it's working")
+                const { board, xIsNext } = state;
+                const { index, winner } = action.payload;
+                if (winner || board[index]) return state;
+                const nextState = JSON.parse(JSON.stringify(state));
+
+                // boardCopy[index] = state.xIsNext ? 'X' : 'O';
+                nextState.board[index] = xIsNext ? 'X' : 'O';
+                nextState.xIsNext = !xIsNext;
+
+                return nextState;
+            }
+        
+        case 'RESET':
+            {
+                const nextState = JSON.parse(JSON.stringify(state));
+                nextState.board = Array(9).fill(null);
+                nextState.xIsNext = true;
+
+                return nextState;
+            }
+
+        default:
+            console.log("Error");
+            break;
+    }
+    return state;
+}
+
+
+
 const Game = () => {
     // const [board, setBoard] = useState(Array(9).fill(null))
     // const [xIsNext, setXIsNext] = useState(true)
-    const [state, setState] = useState({
-        board: Array(9).fill(null),
-        xIsNext: true,
-    })
+    // const [state, setState] = useState({
+    //     board: Array(9).fill(null),
+    //     xIsNext: true,
+    // })
+    
+    // const [state, dispatch] = useReducer(reducer, initialState)
+    // const action = {type: 'CLICK', payload}
+    // dispatch(action) or dispatch({type: 'CLICK'}), needn't to dispatch the function because the value is the object
+    // if we have the data, we should put it in the payload value in the object
+
+
+    const [state, dispatch] = useReducer(gameReducer, initialState)
     const winner = calculateWinner(state.board)
     const handleClick = (index) => {
-        const boardCopy  = [... state.board]
-        if (winner || boardCopy[index]) return;
-        boardCopy[index] = state.xIsNext ? 'X' : 'O';
+        // const boardCopy  = [... state.board]
+        // if (winner || boardCopy[index]) return;
+        dispatch({
+            type: 'CLICK',
+            payload: {
+                index, // index: index,
+                winner,
+            }
+        })
+        // boardCopy[index] = state.xIsNext ? 'X' : 'O';
         // setBoard(boardCopy)
         // setXIsNext((xIsNext) => !xIsNext)
-        setState({
-            board: boardCopy,
-            xIsNext: !state.xIsNext,
-        })
+        // setState({
+        //     board: boardCopy,
+        //     xIsNext: !state.xIsNext,
+        // })
     };
     const handleResetGame = () => {
         // setBoard(Array(9).fill(null))
         // setXIsNext(true); // set X luôn luôn là đánh dấu đầu tiên dù reset game
-        setState({
-            ... state,
-            board: Array(9).fill(null),
-            xIsNext: true,
+        // setState({
+        //     ... state,
+        //     board: Array(9).fill(null),
+        //     xIsNext: true,
+        // })
+        dispatch({
+            type: 'RESET',
         })
     };
 
